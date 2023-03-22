68da0fea18569af899a783393454d12b384f1f8c
nayeemdev <nayeem9812@gmail.com>
2023-03-22 14:01:47 +0600

    feat: added fallback route for api

--- routes/api.php
+++ routes/api.php
@@ -14,6 +14,8 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
+Route::fallback(static function () {
+    return response()->json([
+        'message' => 'URL Not Found.'
+    ], 404);
 });
