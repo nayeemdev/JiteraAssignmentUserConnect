a2eed015a4550ac90e81667587c514931a19bcd9
nayeemdev <nayeem9812@gmail.com>
2023-03-26 01:24:56 +0600

    Update user model and auth guard to use JWT

--- app/Models/User.php
+++ app/Models/User.php
@@ -9,8 +9,9 @@ use Illuminate\Database\Eloquent\Relations\HasOne;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Laravel\Sanctum\HasApiTokens;
+use Tymon\JWTAuth\Contracts\JWTSubject;
 
-class User extends Authenticatable
+class User extends Authenticatable  implements JWTSubject
 {
     use HasApiTokens, HasFactory, Notifiable;
 
@@ -76,4 +77,23 @@ class User extends Authenticatable
         return $this->following()->where('users.id', $user->id)->exists();
     }
 
+    /**
+     * Get the identifier that will be stored in the subject claim of the JWT.
+     *
+     * @return mixed
+     */
+    public function getJWTIdentifier(): mixed
+    {
+        return $this->getKey();
+    }
+
+    /**
+     * Return a key value array, containing any custom claims to be added to the JWT.
+     *
+     * @return array
+     */
+    public function getJWTCustomClaims(): array
+    {
+        return [];
+    }
 }
